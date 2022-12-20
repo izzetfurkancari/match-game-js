2333a05ae66ea1b168a2afd2c90eca869a62cbfe
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-20 03:55:40 +0100

    matching-bk.js 2 || matching-bk.js file setted

--- js/matching-bk.js
+++ js/matching-bk.js
@@ -25,3 +25,35 @@ function getRandomImageForTile() {
 	
 	return iRandomImage;
 }
+function createTile(iCounter) {
+	
+	var curTile =  new tile("tile" + iCounter),
+		iRandomImage = getRandomImageForTile();
+		
+	tileAllocation[iRandomImage] = tileAllocation[iRandomImage] + 1;
+		
+	curTile.setFrontColor("tileColor" + Math.floor((Math.random() * 5) + 1));
+	curTile.setStartAt(500 * Math.floor((Math.random() * 5) + 1));
+	curTile.setFlipMethod(flips[Math.floor((Math.random() * 3) + 1)]);
+	curTile.setBackContentImage("images/" +  (iRandomImage + 1) + ".jpg");
+	
+	return curTile;
+}
+
+function initState() {
+
+	/* Reset the tile allocation count array.  This
+		is used to ensure each image is only 
+		allocated twice.
+	*/
+	tileAllocation = new Array(0,0,0,0,0,0,0,0,0,0);
+	
+	while(tiles.length > 0) {
+		tiles.pop();
+	}
+	
+	$('#board').empty();
+	iTimer = 0;
+	
+}
+
