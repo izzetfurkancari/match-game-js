77f9fcccff96fcdacd0bab26d6c8b8782b2df1c3
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-20 16:51:27 +0100

    tile.js 2 || tile.js setted

--- js/tile.js
+++ js/tile.js
@@ -32,4 +32,63 @@ function tile(id) {
 			this.flipCompleteCallbacks[this.flipCompleteCallbacks.length - 1]();
 			this.flipCompleteCallbacks.pop();
 		}
-	};}
+	};
+    this.revertFlip = function() {
+
+		console.log("Reverting tile " + this.id);
+		
+		$("#" + this.id + " img").hide();
+		
+		$("#" + this.id).revertFlip();
+
+		this.flipped = false;
+	};
+	
+	this.setBackContentImage = function(sBackContentImage) {
+		this.backContentImage = sBackContentImage;
+	};
+	
+	this.setTileId = function(sIdOfTile) {
+		this.id = sIdOfTile;
+	};
+
+	this.setStartAt = function(iStartAt) {
+		this.startAt = iStartAt;
+	};
+	
+	this.setFrontColor = function(sColor) {
+		this.frontColor = sColor;
+	};
+
+	this.setBackColor = function(sColor) {
+		this.backColor = sColor;
+	};
+
+	this.setFlipMethod = function(sFlipMethod) {
+		this.flipMethod = sFlipMethod;
+	};
+	
+	this.getHTML = function() {
+		return '<div id="' + this.id + '" class="tile ' + this.frontColor + '">' + '</div>';
+	};
+
+	this.getStartAt = function() {
+		return this.startAt;
+	};
+
+	this.getFlipped = function() {
+		return this.flipped;
+	};
+	
+	this.getBackContent = function() {
+		return '<img src="' + this.backContentImage + '"/>';
+	};
+
+	this.getBackContentImage = function() {
+		return this.backContentImage;
+	};
+	
+	this.addFlipCompleteCallback = function(callback) {
+		this.flipCompleteCallbacks.push(callback);
+	};
+}
