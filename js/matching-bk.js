67ef53de5b6ebca27ba085cdf301934a40487a5e
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-20 03:43:59 +0100

    matching-bk.js 1 || matching-bk.js file setted

---
+++ js/matching-bk.js
@@ -0,0 +1,27 @@
+var tiles = new Array(),
+	flips = new Array('tb', 'bt', 'lr', 'rl' ),
+	iFlippedTile = null,
+	iTileBeingFlippedId = null,
+	tileImages = new Array(1,2,3,4,5,6,7,8,9,10),
+	tileAllocation = null,
+	iTimer = 0,
+	iInterval = 100,
+	iPeekTime = 3000;
+
+function getRandomImageForTile() {
+
+	var iRandomImage = Math.floor((Math.random() * tileAllocation.length)),
+		iMaxImageUse = 2;
+	
+	while(tileAllocation[iRandomImage] >= iMaxImageUse ) {
+			
+		iRandomImage = iRandomImage + 1;
+			
+		if(iRandomImage >= tileAllocation.length) {
+				
+			iRandomImage = 0;
+		}
+	}
+	
+	return iRandomImage;
+}
